2db5ddd6d31aa079b914c36ab51e68caefd4aff7
Kartik Kumar Pandey <2k23.csai2313425@gmail.com>
2025-07-25 15:51:17 +0530

    Enable PWA support and add favicon/logo

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './App.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -9,4 +10,5 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-); 
+);
+serviceWorkerRegistration.register(); 
